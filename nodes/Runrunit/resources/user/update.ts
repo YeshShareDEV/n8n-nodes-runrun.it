69be7a685c32744a48abb99619de417556a9484a
Marcos Campos <marcos@yesh.com.br>
2026-04-13 12:44:52 -0300

    chore(user): sync create changes to update.ts

--- nodes/Runrunit/resources/user/update.ts
+++ nodes/Runrunit/resources/user/update.ts
@@ -20,12 +20,26 @@ export const userUpdateDescription: INodeProperties[] = [
 		name: 'userObject',
 		type: 'json',
 		displayOptions: { show: showOnlyForUserUpdate },
-		default: '{"name":"","email":"","role":"user"}',
+		default: '{"name":"Colaborador Teste","email":"colaborador.yesh@yesh.com.br","is_manager":false,"team_ids":[435586],"position":"Suporte Nível 1"}',
 		description: 'Full user object or partial object that will be sent as the `user` payload (e.g. {"name":"Maria"})',
 		routing: {
 			send: {
 				type: 'body',
 				property: 'user',
+			},
+		},
+	},
+	{
+		displayName: 'Make Everybody Mutual Partners',
+		name: 'makeEverybodyMutualPartners',
+		type: 'boolean',
+		displayOptions: { show: showOnlyForUserUpdate },
+		default: false,
+		description: 'Whether to make everybody mutual partners (sends as make_everybody_mutual_partners)',
+		routing: {
+			send: {
+				type: 'body',
+				property: 'make_everybody_mutual_partners',
 			},
 		},
 	},
