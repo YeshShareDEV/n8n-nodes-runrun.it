98a4eb6d1740f47d3adc543bf945cc34ff3975f8
Marcos Campos <marcos@yesh.com.br>
2026-04-17 10:33:23 -0300

    ajuate baseUrl na rquisição

--- nodes/Runrunit/Runrunit.node.ts
+++ nodes/Runrunit/Runrunit.node.ts
@@ -170,7 +170,7 @@ export class Runrunit implements INodeType {
 				}
 
 				try {
-					const url = `https://runrun.it/api/v1.0${path}`;
+					const url = `https://runrun.it/api/v1.0/users`;
 					const headers = {
 						'App-Key': appKey,
 						'User-Token': userToken,
